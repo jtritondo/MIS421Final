b52cc78c799e3a7098ad6d2b2507eb569a42c29f
John Ritondo <jtritondo@crimson.ua.edu>
2020-04-13 11:54:45 -0500

    Add project files.

---
+++ MIS421Final/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MIS421Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
